ba819b33f0eb344e59d5a207574a17a4f0ead864
liull <liull@asiainfo.com>
2019-04-06 00:47:17 +0800

    初次创建

---
+++ src/main/java/com/sunian/cloud/SunianCloudApplication.java
@@ -0,0 +1,13 @@
+package com.sunian.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunianCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunianCloudApplication.class, args);
+	}
+
+}
