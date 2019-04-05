6ebc3956fb939ae27e98881d67c5534f68f15a8d
liull <liull@asiainfo.com>
2019-04-06 01:24:38 +0800

    将springboot改进为springcloud项目

--- cloud-service/user-cloud/src/main/java/com/sunian/cloud/UserCloudApplication.java
+++ cloud-service/user-cloud/src/main/java/com/sunian/cloud/UserCloudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SunianCloudApplication {
+public class UserCloudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SunianCloudApplication.class, args);
+		SpringApplication.run(UserCloudApplication.class, args);
 	}
 
 }
--- cloud-service/user-cloud/src/test/java/com/sunian/cloud/UserCloudApplicationTests.java
+++ cloud-service/user-cloud/src/test/java/com/sunian/cloud/UserCloudApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SunianCloudApplicationTests {
+public class UserCloudApplicationTests {
 
 	@Test
 	public void contextLoads() {
