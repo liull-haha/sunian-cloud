ff9a3f0201333a27bbeb1f6adfd017ba2e6abd03
liull <liull@asiainfo.com>
2020-12-04 10:09:29 +0800

    添加gateway配置

--- cloud-blog/src/main/java/com/sunian/controller/blogController.java
+++ cloud-blog/src/main/java/com/sunian/controller/blogController.java
@@ -3,6 +3,7 @@ package com.sunian.controller;
 import com.sunian.openFeign.UserOpenFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by liull on 2019/7/10.
  */
 @RestController
+@RequestMapping(value = "/blog")
 public class blogController {
     @Autowired
     UserOpenFeignService userOpenFeignService;
 
-    @PostMapping(value = "/blog/sayBlog")
+    @PostMapping(value = "/sayBlog")
     public String sayBlog(@RequestParam("name")String name){
         return userOpenFeignService.sayUser(name);
     }
